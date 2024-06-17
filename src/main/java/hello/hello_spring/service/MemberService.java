8c5b807c41fb153746f38aeb1a398bb201046bf7
user <whs1103@naver.com>
2024-06-17 15:17:36 +0900

    ㅇ

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -15,6 +15,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
 
+
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
